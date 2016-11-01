f189d1a85e66dfa8560c997dbd6f4259c0f8c81e
Wasikemarvine <jayelwasike@gmail.com>
2016-11-01 16:26:53 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,9 @@
-def 
+def get_user():
+	username=input("Whats your name?")
+	profile_info = requests.get('https://api.github.com/users/'+ username)
+
+	print (profile)
+
+	for info in profile:
+		pass
+get_user()
